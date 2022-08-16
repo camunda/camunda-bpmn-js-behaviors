94ad00e8dfa0227258876f810c5604a0ac923775
Martin Stamm <martin.stamm@camunda.com>
2022-08-16 09:37:16 +0200

    feat: keep executeable flag after participant deletion
    related to https://github.com/camunda/camunda-bpmn-js/issues/149

---
+++ test/camunda-platform/DeleteParticipantBehaviorSpec.js
@@ -0,0 +1,61 @@
+import {
+  bootstrapCamundaPlatformModeler,
+  inject
+} from 'test/TestHelper';
+
+import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
+
+import diagramXML from './camunda-executable-participant.bpmn';
+
+
+describe('camunda-platform/features/modeling - DeleteParticipantBehaviour', function() {
+
+  beforeEach(bootstrapCamundaPlatformModeler(diagramXML));
+
+  describe('remove set isExecuteable on new Process', function() {
+
+    beforeEach(inject(function(elementRegistry, modeling) {
+
+      // given
+      const shape = elementRegistry.get('Participant_1');
+
+      // when
+      modeling.removeShape(shape);
+    }));
+
+
+    it('should execute', inject(function(canvas) {
+
+      // then
+      const newRoot = getBusinessObject(canvas.getRootElement());
+      expect(newRoot.isExecutable).to.be.true;
+    }));
+
+
+    it('should undo', inject(function(canvas, commandStack) {
+
+      // given
+      const newRoot = getBusinessObject(canvas.getRootElement());
+
+      // when
+      commandStack.undo();
+
+      // then
+      expect(newRoot.isExecutable).not.to.exist;
+    }));
+
+
+    it('should redo', inject(function(canvas, commandStack) {
+
+      // when
+      commandStack.undo();
+      commandStack.redo();
+
+      // then
+      const newRoot = getBusinessObject(canvas.getRootElement());
+      expect(newRoot.isExecutable).to.be.true;
+    }));
+
+  });
+
+});
