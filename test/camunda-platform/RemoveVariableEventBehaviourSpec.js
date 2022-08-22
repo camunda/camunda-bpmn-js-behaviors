7ef3f5c00e0beaef4069c39023ea0e1973e93811
Philipp <philippfromme@outlook.com>
2022-08-22 15:17:53 +0200

    feat(camunda-platform): add behaviors from camunda-bpmn-moddle
    Related to https://github.com/camunda/camunda-bpmn-js-behaviors/issues/4
    Related to https://github.com/camunda/camunda-bpmn-moddle/issues/110

---
+++ test/camunda-platform/RemoveVariableEventBehaviourSpec.js
@@ -0,0 +1,116 @@
+import {
+  bootstrapCamundaPlatformModeler,
+  inject
+} from 'test/TestHelper';
+
+import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
+
+
+
+import diagramXML from './camunda-variable-event.bpmn';
+
+
+describe('browser - RemoveVariableEventBehaviour', function() {
+
+  beforeEach(bootstrapCamundaPlatformModeler(diagramXML));
+
+
+  describe('remove variableEvents property', function() {
+
+    describe('when conditional startEvent is moved out of event subprocess', function() {
+
+      var startEvent,
+          startBusinessObject,
+          eventSubProcess,
+          eventDefinitions,
+          root;
+
+      it('should not have an variableEvents property', inject(function(elementRegistry, modeling, canvas) {
+
+        // given
+        root = canvas.getRootElement();
+        startEvent = elementRegistry.get('startEvent_1');
+        startBusinessObject = getBusinessObject(startEvent);
+        eventDefinitions = startBusinessObject.get('eventDefinitions');
+        eventSubProcess = elementRegistry.get('Activity_subprocess1');
+
+
+        // assume
+        eventDefinitions.forEach(def => {
+          expect(def.get('camunda:variableEvents')).to.not.be.undefined;
+        });
+
+        // when
+        modeling.moveShape(startEvent, { x: eventSubProcess.width, y: eventSubProcess.height }, root);
+
+        eventDefinitions = startBusinessObject.get('eventDefinitions');
+
+        // then
+        eventDefinitions.forEach(def => {
+          expect(def.get('camunda:variableEvents')).to.be.undefined;
+        });
+      }));
+
+    });
+
+
+    describe('when conditional startEvent is moved out to another event subprocess', function() {
+
+      var startEvent,
+          startBusinessObject,
+          eventSubProcess,
+          eventDefinitions;
+
+      it('should maintain variableEvents property', inject(function(elementRegistry, modeling, canvas) {
+
+        // given
+        startEvent = elementRegistry.get('startEvent_1');
+        startBusinessObject = getBusinessObject(startEvent);
+        eventDefinitions = startBusinessObject.get('eventDefinitions');
+        eventSubProcess = elementRegistry.get('Activity_subprocess2');
+
+
+        // assume
+        eventDefinitions.forEach(def => {
+          expect(def.get('camunda:variableEvents')).to.not.be.undefined;
+        });
+
+        // when
+        modeling.moveShape(startEvent, { x: 0, y: eventSubProcess.height }, eventSubProcess);
+
+        // then
+        eventDefinitions.forEach(def => {
+          expect(def.get('camunda:variableEvents')).to.not.be.undefined;
+        });
+      }));
+
+    });
+
+
+    describe('when conditional startEvent with variableEvents property is created out of event subprocess', function() {
+
+      var root,
+          eventBusinessObject,
+          eventDefinition;
+
+      it('should not have variableEvents property', inject(function(canvas, modeling, bpmnFactory) {
+
+        // given
+        root = canvas.getRootElement();
+        eventDefinition = bpmnFactory.create('bpmn:ConditionalEventDefinition', { variableEvents:'abc' });
+        eventBusinessObject = bpmnFactory.create('bpmn:StartEvent', { eventDefinitions: [eventDefinition] });
+
+        // when
+        modeling.createShape({ type: 'bpmn:StartEvent' , businessObject: eventBusinessObject }, { x: 0, y: 0 }, root);
+
+        // then
+        eventBusinessObject.get('eventDefinitions').forEach(def => {
+          expect(def.get('camunda:variableEvents')).to.be.undefined;
+        });
+      }));
+
+    });
+
+  });
+
+});
