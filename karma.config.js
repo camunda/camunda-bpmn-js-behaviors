7a201d1cc93402e8f391d656025cd651f3dcfbb0
Nico Rehwaldt <git_nikku@nixis.de>
2022-08-24 08:55:40 +0200

    deps: upgrade to webpack@5
    * drop `raw-loader`
    * use modern coverage reporting

--- karma.config.js
+++ karma.config.js
@@ -57,17 +57,24 @@ module.exports = function(karma) {
         rules: [
           {
             test: /\.(css|bpmn)$/,
-            use: 'raw-loader'
+            type: 'asset/source'
           }
-        ].concat(coverage ?
-          {
+        ].concat(
+          coverage ? {
             test: /\.js$/,
+            exclude: /node_modules/,
             use: {
-              loader: 'istanbul-instrumenter-loader',
-              options: { esModules: true }
-            },
-            include: /lib\.*/,
-            exclude: /node_modules/
+              loader: 'babel-loader',
+              options: {
+                plugins: [
+                  [ 'istanbul', {
+                    include: [
+                      'lib/**'
+                    ]
+                  } ]
+                ],
+              }
+            }
           } : []
         )
       },
