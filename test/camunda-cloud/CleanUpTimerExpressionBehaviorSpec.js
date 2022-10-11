d748d3244813ae5cbe5d660ae1cfb15d96fd96e7
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2022-10-11 10:51:42 +0200

    feat: clean up timer expressions
    Related to https://github.com/camunda/camunda-modeler/issues/2368

---
+++ test/camunda-cloud/CleanUpTimerExpressionBehaviorSpec.js
@@ -0,0 +1,133 @@
+import {
+  bootstrapCamundaCloudModeler,
+  inject
+} from 'test/TestHelper';
+
+import {
+  getBusinessObject,
+  is
+} from 'bpmn-js/lib/util/ModelUtil';
+
+import diagramXML from './process-timer.bpmn';
+
+
+describe('camunda-cloud/features/modeling - CleanUpTimerExpressionBehavior', function() {
+
+  beforeEach(bootstrapCamundaCloudModeler(diagramXML));
+
+
+  describe('move', function() {
+
+    it('should remove timeCycle when start event is moved to event subprocess', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        const startEvent = elementRegistry.get('StartEventCycle'),
+              eventSubprocess = elementRegistry.get('EventSubProcess');
+
+        // when
+        modeling.moveElements([ startEvent ], { x: eventSubprocess.x + 20, y: eventSubprocess.y + 20 }, eventSubprocess);
+
+        // then
+        const timerEventDefinition = getTimerEventDefinition(startEvent);
+
+        expect(timerEventDefinition.get('timeCycle')).not.to.exist;
+      }
+    ));
+
+
+    it('should NOT remove timeCycle when moving start event around process', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        const startEvent = elementRegistry.get('StartEventCycle'),
+              process = elementRegistry.get('Process');
+
+        // when
+        modeling.moveElements([ startEvent ], { x: startEvent.x + 20, y: startEvent.y + 20 }, process);
+
+        // then
+        const timerEventDefinition = getTimerEventDefinition(startEvent);
+
+        expect(timerEventDefinition.get('timeCycle')).to.exist;
+      }
+    ));
+
+  });
+
+
+  describe('replace', function() {
+
+    it('should remove timeCycle when boundary event is replaced with an interrupting one', inject(
+      function(elementRegistry, bpmnReplace) {
+
+        // given
+        let boundaryEvent = elementRegistry.get('NonInterruptingBoundaryEvent');
+
+        // when
+        bpmnReplace.replaceElement(boundaryEvent, {
+          type: 'bpmn:BoundaryEvent',
+          eventDefinitionType: 'bpmn:TimerEventDefinition',
+          cancelActivity: true
+        });
+
+        // then
+        boundaryEvent = elementRegistry.get('NonInterruptingBoundaryEvent');
+        const timerEventDefinition = getTimerEventDefinition(boundaryEvent);
+
+        expect(timerEventDefinition.get('timeCycle')).not.to.exist;
+      }
+    ));
+  });
+
+
+  describe('update', function() {
+
+    it('should remove time cycle when `modeling.updateProperties` makes it disallowed', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        const boundaryEvent = elementRegistry.get('NonInterruptingBoundaryEvent');
+
+        // when
+        modeling.updateProperties(boundaryEvent, {
+          cancelActivity: undefined
+        });
+
+        // then
+        const timerEventDefinition = getTimerEventDefinition(boundaryEvent);
+
+        expect(timerEventDefinition.get('timeCycle')).not.to.exist;
+      }
+    ));
+
+
+    it('should remove time cycle when `modeling.updateModdleProperties` makes it disallowed', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        const boundaryEvent = elementRegistry.get('NonInterruptingBoundaryEvent');
+
+        // when
+        let timerEventDefinition = getTimerEventDefinition(boundaryEvent);
+        modeling.updateModdleProperties(boundaryEvent, getBusinessObject(boundaryEvent), {
+          cancelActivity: undefined
+        });
+
+        // then
+        timerEventDefinition = getTimerEventDefinition(boundaryEvent);
+        expect(timerEventDefinition.get('timeCycle')).not.to.exist;
+      }
+    ));
+  });
+});
+
+
+
+// helper //////////////
+
+function getTimerEventDefinition(element) {
+  const bo = getBusinessObject(element);
+
+  return bo.eventDefinitions.find(def => is(def, 'bpmn:TimerEventDefinition'));
+}
