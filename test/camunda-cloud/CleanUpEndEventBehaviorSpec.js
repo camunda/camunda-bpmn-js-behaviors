60c418e8eeff7b71af27db7627fb3f46edc70273
Martin Stamm <martin.stamm@camunda.com>
2023-08-21 13:20:22 +0200

    feat(cloud): remove output mappings from error end events
    related to camunda/camunda-modeler#3782

---
+++ test/camunda-cloud/CleanUpEndEventBehaviorSpec.js
@@ -0,0 +1,75 @@
+import {
+  bootstrapCamundaCloudModeler,
+  inject
+} from 'test/TestHelper';
+
+import { find } from 'min-dash';
+
+import {
+  getBusinessObject,
+  is
+} from 'bpmn-js/lib/util/ModelUtil';
+
+import diagramXML from './process-end-event.bpmn';
+
+
+describe('camunda-cloud/features/modeling - CleanUpEndEventBehavior', function() {
+
+  beforeEach(bootstrapCamundaCloudModeler(diagramXML));
+
+  describe('replace', function() {
+
+    it('should remove timeCycle when boundary event is replaced with an interrupting one', inject(
+      function(elementRegistry, bpmnReplace) {
+
+        // given
+        let endEvent = elementRegistry.get('EndEvent_1');
+
+        // assume
+        expect(getOutputs(endEvent)).to.have.length(1);
+
+        // when
+        bpmnReplace.replaceElement(endEvent, {
+          type: 'bpmn:EndEvent',
+          eventDefinitionType: 'bpmn:ErrorEventDefinition'
+        });
+
+        // then
+        endEvent = elementRegistry.get('EndEvent_1');
+        expect(getOutputs(endEvent)).to.have.length(0);
+      }
+    ));
+  });
+
+
+});
+
+
+// helpers //////////
+
+function getOutputs(element) {
+  const ioMapping = getIoMapping(element);
+  if (!ioMapping) {
+    return [];
+  }
+
+  return ioMapping.get('outputParameters');
+}
+
+function getIoMapping(element) {
+  const bo = getBusinessObject(element);
+
+  const extensionElements = bo.get('extensionElements');
+
+  if (!extensionElements) {
+    return null;
+  }
+
+  const values = extensionElements.get('values');
+
+  if (!values) {
+    return null;
+  }
+
+  return find(values, value => is(value, 'zeebe:IoMapping'));
+}
