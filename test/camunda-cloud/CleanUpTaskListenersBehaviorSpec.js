66618e92198d9990d323d02a799c30dde1ba3c8d
Michał Konopski <michal.konopski@camunda.com>
2024-11-11 15:48:54 +0100

    feat: support `zeebe:TaskListeners`
    related to https://github.com/camunda/camunda-modeler/issues/4590

---
+++ test/camunda-cloud/CleanUpTaskListenersBehaviorSpec.js
@@ -0,0 +1,191 @@
+import {
+  bootstrapCamundaCloudModeler,
+  inject
+} from 'test/TestHelper';
+
+import { getExtensionElementsList } from 'lib/util/ExtensionElementsUtil';
+
+import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
+
+import diagramXML from './task-listeners.bpmn';
+
+
+describe('camunda-cloud/features/modeling - CleanUpTaskListenersBehavior', function() {
+
+  beforeEach(bootstrapCamundaCloudModeler(diagramXML));
+
+  describe('remove execution listeners if disallowed in element type', function() {
+
+    const testCases = [
+      {
+        title: 'User Task -> Start Event',
+        element: 'UserTask',
+        target: {
+          type: 'bpmn:StartEvent'
+        }
+      },
+      {
+        title: 'User Task -> Gateway',
+        element: 'UserTask',
+        target: {
+          type: 'bpmn:Gateway'
+        }
+      }
+    ];
+
+    for (const { title, element, target } of testCases) {
+
+      describe(title, function() {
+
+        it('should execute', inject(function(bpmnReplace, elementRegistry) {
+
+          // given
+          let el = elementRegistry.get(element);
+
+          // when
+          bpmnReplace.replaceElement(el, target);
+
+          // then
+          el = elementRegistry.get(element);
+
+          const executionListenersContainer = getTaskListenersContainer(el);
+
+          expect(executionListenersContainer).not.to.exist;
+        }));
+
+
+        it('should undo', inject(function(bpmnReplace, commandStack, elementRegistry) {
+
+          // given
+          let el = elementRegistry.get(element);
+
+          // when
+          bpmnReplace.replaceElement(el, target);
+
+          commandStack.undo();
+
+          // then
+          el = elementRegistry.get(element);
+          const extensionElements = getBusinessObject(el).get('extensionElements');
+
+          expect(extensionElements.get('values')).to.have.lengthOf(2);
+        }));
+
+
+        it('should redo', inject(function(bpmnReplace, commandStack, elementRegistry) {
+
+          // given
+          let el = elementRegistry.get(element);
+
+          // when
+          bpmnReplace.replaceElement(el, target);
+
+          commandStack.undo();
+          commandStack.redo();
+
+          // then
+          el = elementRegistry.get(element);
+          const executionListenersContainer = getTaskListenersContainer(el);
+
+          expect(executionListenersContainer).not.to.exist;
+        }));
+      });
+    }
+  });
+
+  describe('should remove execution listeners of disallowed type', function() {
+
+    it('should execute', inject(function(bpmnReplace, elementRegistry) {
+
+      // given
+      let el = elementRegistry.get('UserTaskWrongType');
+      let targetEl = elementRegistry.get('UserTask');
+
+      // when
+      bpmnReplace.replaceElement(el, targetEl);
+
+      // then
+      el = elementRegistry.get('UserTaskWrongType');
+      const container = getExtensionElementsList(getBusinessObject(el), 'zeebe:TaskListeners')[0];
+
+      expect(container.get('listeners')).to.have.lengthOf(1);
+    }));
+
+
+    it('should undo', inject(function(bpmnReplace, commandStack, elementRegistry) {
+
+      // given
+      let el = elementRegistry.get('UserTaskWrongType');
+      let targetEl = elementRegistry.get('UserTask');
+
+      // when
+      bpmnReplace.replaceElement(el, targetEl);
+
+      commandStack.undo();
+
+      // then
+      el = elementRegistry.get('UserTaskWrongType');
+      const container = getExtensionElementsList(getBusinessObject(el), 'zeebe:TaskListeners')[0];
+
+      expect(container.get('listeners')).to.have.lengthOf(2);
+    }));
+
+
+    it('should redo', inject(function(bpmnReplace, commandStack, elementRegistry) {
+
+      // given
+      let el = elementRegistry.get('UserTaskWrongType');
+      let targetEl = elementRegistry.get('UserTask');
+
+      // when
+      bpmnReplace.replaceElement(el, targetEl);
+
+      commandStack.undo();
+      commandStack.redo();
+
+      // then
+      el = elementRegistry.get('UserTaskWrongType');
+      const container = getExtensionElementsList(getBusinessObject(el), 'zeebe:TaskListeners')[0];
+
+      expect(container.get('listeners')).to.have.lengthOf(1);
+    }));
+
+
+    it('should remove zeebe:TaskListeners for non zeebe user task', inject(function(bpmnReplace, elementRegistry) {
+
+      // given
+      let el = elementRegistry.get('NonZeebeUserTask');
+      const targetEl = elementRegistry.get('UserTask');
+
+      // when
+      bpmnReplace.replaceElement(el, targetEl);
+
+      // then
+      el = elementRegistry.get('NonZeebeUserTask');
+      const extensionElements = getBusinessObject(el).get('extensionElements');
+      console.log(extensionElements.get('values'));
+
+      expect(extensionElements.get('values')).to.have.lengthOf(0);
+    }));
+
+
+    it('should remove zeebe:TaskListeners', inject(function(elementRegistry, modeling) {
+
+      // given
+      const el = elementRegistry.get('UserTask');
+      const listenersContainer = getTaskListenersContainer(el);
+
+      // when
+      modeling.updateModdleProperties(el, listenersContainer, { listeners: [] });
+
+      // then
+      const extensionElements = getBusinessObject(el).get('extensionElements');
+
+      expect(extensionElements.get('values')).to.have.lengthOf(1);
+    }));
+  });
+});
+
+function getTaskListenersContainer(element) {
+  return getExtensionElementsList(getBusinessObject(element), 'zeebe:TaskListeners')[0];
+}
