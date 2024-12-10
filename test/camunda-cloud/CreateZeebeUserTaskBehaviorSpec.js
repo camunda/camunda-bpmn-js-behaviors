dc7860161a444dd1708a91170b57b91a1baea8a7
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2024-12-10 17:58:40 +0100

    chore: refactor with code review results in mind

--- test/camunda-cloud/CreateZeebeUserTaskBehaviorSpec.js
+++ test/camunda-cloud/CreateZeebeUserTaskBehaviorSpec.js
@@ -1,20 +1,20 @@
 import { bootstrapCamundaCloudModeler, inject } from 'test/TestHelper';
 
+import { getBusinessObject, is } from 'bpmn-js/lib/util/ModelUtil';
 import { find } from 'min-dash';
 
-import {
-  getZeebeUserTaskElement,
-  getZeebeUserTaskElements,
-} from '../../lib/camunda-cloud/util/ZeebeUserTaskUtil';
+import { getExtensionElementsList } from 'lib/util/ExtensionElementsUtil';
 
-import { getBusinessObject, is } from 'bpmn-js/lib/util/ModelUtil';
 
 import emptyProcessDiagramXML from './process-empty.bpmn';
 import userTasksXML from './process-user-tasks.bpmn';
 
 describe('camunda-cloud/features/modeling - CreateZeebeUserTaskBehavior', function() {
-  describe('when creating new shapes', function() {
+
+  describe('when a shape is created', function() {
+
     beforeEach(bootstrapCamundaCloudModeler(emptyProcessDiagramXML));
+
 
     it('should execute when creating bpmn:UserTask', inject(function(
         canvas,
@@ -33,15 +33,44 @@ describe('camunda-cloud/features/modeling - CreateZeebeUserTaskBehavior', functi
 
       // then
       const businessObject = getBusinessObject(newShape),
-            extensionElements = businessObject.get('extensionElements'),
-            zeebeUserTaskExtension = getZeebeUserTaskElement(newShape);
+            zeebeUserTaskExtensions = getExtensionElementsList(businessObject, 'zeebe:UserTask');
 
-      expect(zeebeUserTaskExtension).to.exist;
-      expect(extensionElements).to.exist;
-      expect(zeebeUserTaskExtension.$parent).to.equal(extensionElements);
+      expect(zeebeUserTaskExtensions).to.exist;
+      expect(zeebeUserTaskExtensions).to.have.lengthOf(1);
     }));
 
-    it('should not execute when creating bpmn:Task', inject(function(
+
+    it('should NOT execute when zeebe:UserTask already present', inject(function(
+        canvas,
+        bpmnFactory,
+        modeling
+    ) {
+
+      // given
+      const rootElement = canvas.getRootElement(),
+            bo = bpmnFactory.create('bpmn:UserTask', {
+              extensionElements: bpmnFactory.create('bpmn:ExtensionElements', {
+                values: [ bpmnFactory.create('zeebe:UserTask') ],
+              }),
+            });
+
+      // when
+      const newShape = modeling.createShape(
+        { type: 'bpmn:UserTask', businessObject: bo },
+        { x: 100, y: 100 },
+        rootElement
+      );
+
+      // then
+      const businessObject = getBusinessObject(newShape),
+            zeebeUserTaskExtensions = getExtensionElementsList(businessObject, 'zeebe:UserTask');
+
+      expect(zeebeUserTaskExtensions).to.exist;
+      expect(zeebeUserTaskExtensions).to.have.lengthOf(1);
+    }));
+
+
+    it('should NOT execute when creating bpmn:Task', inject(function(
         canvas,
         modeling
     ) {
@@ -57,16 +86,51 @@ describe('camunda-cloud/features/modeling - CreateZeebeUserTaskBehavior', functi
       );
 
       // then
-      const zeebeUserTaskExtension = getZeebeUserTaskElement(newShape);
+      const zeebeUserTaskExtension = getZeebeUserTask(newShape);
 
       expect(zeebeUserTaskExtension).not.to.exist;
     }));
   });
 
-  describe('when copying bpmn:UserTask', function() {
+
+  describe('when a shape is pasted', function() {
+
     beforeEach(bootstrapCamundaCloudModeler(userTasksXML));
 
-    it('should re-use existing extensionElement', inject(function(
+
+    it('should NOT add zeebe:UserTask', inject(function(
+        canvas,
+        copyPaste,
+        elementRegistry
+    ) {
+
+      // given
+      const rootElement = canvas.getRootElement();
+      const userTask = elementRegistry.get('UserTask_1');
+
+      // when
+      copyPaste.copy(userTask);
+
+      const elements = copyPaste.paste({
+        element: rootElement,
+        point: {
+          x: 1000,
+          y: 1000,
+        },
+      });
+
+      // then
+      const pastedUserTask = find(elements, (element) =>
+        is(element, 'bpmn:UserTask')
+      );
+
+      const zeebeUserTask = getZeebeUserTask(pastedUserTask);
+
+      expect(zeebeUserTask).not.to.exist;
+    }));
+
+
+    it('should keep existing zeebe:UserTask', inject(function(
         canvas,
         copyPaste,
         elementRegistry
@@ -91,57 +155,20 @@ describe('camunda-cloud/features/modeling - CreateZeebeUserTaskBehavior', functi
       const pastedUserTask = find(elements, (element) =>
         is(element, 'bpmn:UserTask')
       );
+      const zeebeUserTasks = getExtensionElementsList(pastedUserTask, 'zeebe:UserTask');
 
-      const businessObject = getBusinessObject(pastedUserTask),
-            extensionElements = businessObject.get('extensionElements'),
-            zeebeUserTaskExtensions = getZeebeUserTaskElements(pastedUserTask);
-
-      expect(zeebeUserTaskExtensions).to.exist;
-      expect(extensionElements).to.exist;
-      expect(zeebeUserTaskExtensions.length).to.equal(1);
-      expect(zeebeUserTaskExtensions[0].$parent).to.equal(extensionElements);
-    }));
-
-    it('should not add zeebe:UserTask if it was not already present', inject(function(
-        canvas,
-        copyPaste,
-        elementRegistry
-    ) {
-
-      // given
-      const rootElement = canvas.getRootElement();
-      const userTask = elementRegistry.get('withEmptyExternalReference');
-
-      // when
-      copyPaste.copy(userTask);
-
-      const elements = copyPaste.paste({
-        element: rootElement,
-        point: {
-          x: 1000,
-          y: 1000,
-        },
-      });
-
-      // then
-      const pastedUserTask = find(elements, (element) =>
-        is(element, 'bpmn:UserTask')
-      );
-
-      const businessObject = getBusinessObject(pastedUserTask),
-            extensionElements = businessObject.get('extensionElements'),
-            zeebeUserTaskExtensions = getZeebeUserTaskElements(pastedUserTask);
-
-      expect(zeebeUserTaskExtensions).to.exist;
-      expect(extensionElements).to.exist;
-      expect(zeebeUserTaskExtensions.length).to.equal(0);
+      expect(zeebeUserTasks).to.exist;
+      expect(zeebeUserTasks).to.have.lengthOf(1);
     }));
   });
 
-  describe('when replacing bpmn:Task', function() {
+
+  describe('when a shape is replaced', function() {
+
     beforeEach(bootstrapCamundaCloudModeler(userTasksXML));
 
-    it('should execute when replacing to bpmn:UserTask', inject(function(
+
+    it('should add zeebe:UserTask when target is bpmn:UserTask', inject(function(
         elementRegistry,
         bpmnReplace,
         canvas,
@@ -161,9 +188,52 @@ describe('camunda-cloud/features/modeling - CreateZeebeUserTaskBehavior', functi
 
       // then
       const updatedTask = elementRegistry.get(task.id),
-            zeebeUserTaskExtension = getZeebeUserTaskElement(updatedTask);
+            zeebeUserTaskExtension = getZeebeUserTask(updatedTask);
 
       expect(zeebeUserTaskExtension).to.exist;
     }));
+
+
+    it('should NOT add zeebe:UserTask when target is bpmn:ServiceTask', inject(function(
+        elementRegistry,
+        bpmnReplace,
+        canvas,
+        modeling
+    ) {
+
+      // given
+      const rootElement = canvas.getRootElement();
+
+      // when
+      const task = modeling.createShape(
+        { type: 'bpmn:Task', id: 'simpleTask' },
+        { x: 100, y: 100 },
+        rootElement
+      );
+      bpmnReplace.replaceElement(task, { type: 'bpmn:ServiceTask' });
+
+      // then
+      const updatedTask = elementRegistry.get(task.id),
+            zeebeUserTask = getZeebeUserTask(updatedTask);
+
+      expect(zeebeUserTask).not.to.exist;
+    }));
   });
 });
+
+
+// helpers //////////
+
+/**
+ * Get the first zeebe:userTask element of an element.
+ *
+ * @param {djs.model.Base|ModdleElement} element
+ *
+ * @returns {ModdleElement|null}
+ */
+function getZeebeUserTask(element) {
+  const businessObject = getBusinessObject(element);
+  const userTaskElements = getExtensionElementsList(businessObject, 'zeebe:UserTask');
+
+  return userTaskElements[0] || null;
+}
