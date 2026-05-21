8f168f520f103d850a7ed0a4373a1e1c80f5a229
AlekseyManetov <aleksey.manetov@camunda.com>
2026-05-21 19:26:29 +0200

    fix: fix start event form behavior with copy/paste
    Fixed the form definition and user task form cleanup when paste start event into the subprocess.
    Create new user form reference when copy/paste start event with embedded form.
    
    Related to https://github.com/camunda/camunda-modeler/issues/4672

--- test/camunda-cloud/StartEventFormsBehaviorSpec.js
+++ test/camunda-cloud/StartEventFormsBehaviorSpec.js
@@ -11,6 +11,7 @@ import { getExtensionElementsList } from 'lib/util/ExtensionElementsUtil';
 
 import {
   getFormDefinition,
+  getUserTaskForm,
 } from 'lib/camunda-cloud/util/FormsUtil';
 
 import diagramXML from './process-start-event-forms.bpmn';
@@ -106,6 +107,31 @@ describe('camunda-cloud/features/modeling - FormsBehavior - start events', funct
       ));
 
     });
+
+  });
+
+
+  describe('remove start event label', function() {
+
+    it('should not remove user task form when label is deleted', inject(
+      function(canvas, elementRegistry, modeling) {
+
+        // given
+        const rootElement = canvas.getRootElement();
+        const element = elementRegistry.get('StartEvent_Embedded');
+        const label = element.label;
+
+        // when
+        modeling.removeElements([ label ]);
+
+        // then
+        const userTaskForms = getExtensionElementsList(
+          getBusinessObject(rootElement), 'zeebe:UserTaskForm'
+        );
+
+        expect(userTaskForms).to.have.lengthOf(1);
+      }
+    ));
 
   });
 
@@ -312,6 +338,134 @@ describe('camunda-cloud/features/modeling - FormsBehavior - start events', funct
           const formDefinition = getFormDefinition(element);
 
           expect(formDefinition).to.exist;
+
+          const userTaskForms = getExtensionElementsList(
+            getBusinessObject(rootElement), 'zeebe:UserTaskForm'
+          );
+
+          expect(userTaskForms).to.have.lengthOf(1);
+        }
+      ));
+
+    });
+
+  });
+
+
+  describe('paste start event at process root', function() {
+
+    describe('with linked form', function() {
+
+      it('should keep form definition', inject(
+        function(canvas, elementRegistry, copyPaste) {
+
+          // given
+          const rootElement = canvas.getRootElement();
+
+          // when
+          copyPaste.copy(elementRegistry.get('StartEvent_Linked'));
+
+          const pastedElement = copyPaste.paste({
+            element: rootElement,
+            point: { x: 200, y: 200 }
+          })[ 0 ];
+
+          // then
+          const formDefinition = getFormDefinition(pastedElement);
+
+          expect(formDefinition).to.exist;
+          expect(formDefinition.get('formId')).to.equal('myFormId');
+        }
+      ));
+
+    });
+
+
+    describe('with embedded form', function() {
+
+      it('should keep form definition and create new user task form reference', inject(
+        function(canvas, elementRegistry, copyPaste) {
+
+          // given
+          const rootElement = canvas.getRootElement();
+
+          // when
+          copyPaste.copy(elementRegistry.get('StartEvent_Embedded'));
+
+          const pastedElement = copyPaste.paste({
+            element: rootElement,
+            point: { x: 200, y: 200 }
+          })[ 0 ];
+
+          // then
+          const formDefinition = getFormDefinition(pastedElement);
+
+          expect(formDefinition).to.exist;
+
+          const userTaskForm = getUserTaskForm(pastedElement);
+
+          expect(userTaskForm).to.exist;
+          expect(userTaskForm.get('id')).to.not.equal('UserTaskForm_1');
+
+          const userTaskForms = getExtensionElementsList(
+            getBusinessObject(rootElement), 'zeebe:UserTaskForm'
+          );
+
+          expect(userTaskForms).to.have.lengthOf(2);
+        }
+      ));
+
+    });
+
+  });
+
+
+  describe('paste start event into subprocess', function() {
+
+    describe('with linked form', function() {
+
+      it('should remove form definition', inject(
+        function(elementRegistry, copyPaste) {
+
+          // given
+          const startEvent = elementRegistry.get('StartEvent_Linked');
+          const subProcess = elementRegistry.get('SubProcess_1');
+
+          // when
+          copyPaste.copy(startEvent);
+
+          const pastedElement = copyPaste.paste({
+            element: subProcess,
+            point: { x: 100, y: 100 }
+          })[ 0 ];
+
+          // then
+          expect(getFormDefinition(pastedElement)).not.to.exist;
+        }
+      ));
+
+    });
+
+
+    describe('with embedded form', function() {
+
+      it('should remove form definition and user task form', inject(
+        function(canvas, elementRegistry, copyPaste) {
+
+          // given
+          const rootElement = canvas.getRootElement();
+          const subProcess = elementRegistry.get('SubProcess_1');
+
+          // when
+          copyPaste.copy(elementRegistry.get('StartEvent_Embedded'));
+
+          const pastedElement = copyPaste.paste({
+            element: subProcess,
+            point: { x: 100, y: 100 }
+          })[ 0 ];
+
+          // then
+          expect(getFormDefinition(pastedElement)).not.to.exist;
 
           const userTaskForms = getExtensionElementsList(
             getBusinessObject(rootElement), 'zeebe:UserTaskForm'
